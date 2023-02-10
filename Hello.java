9449fa7278dfccd5879db0942409a807c9619a41
IamPawan777 <iampawanbisht7@gmail.com>
2023-02-11 00:05:32 +0530

    First file

---
+++ Hello.java
@@ -0,0 +1,7 @@
+class Hello {
+    public static void main(String[] args) {
+        int no = 5;
+        System.out.println("No value is: "+no);
+        System.out.println("No value is: "+no);
+    }
+}
