f360df15e19395a9756572ffa8078e18443c61d1
IamPawan777 <iampawanbisht7@gmail.com>
2023-02-17 18:40:34 +0530

    changes

--- TypeCasting.java
+++ TypeCasting.java
@@ -7,15 +7,15 @@ public class TypeCasting {
         
         int value = 2323;
         double value2;
-    value2 = value;             //implicit type casting
+    value2 = value;                  //implicit type casting
         System.out.println(value2);
 
         double b = 121.2222;
-    int a = (int)b;             //explicit type casting
+    int a = (int)b;                  //explicit type casting
         System.out.println(a);
 
         int p = 12; int q = 5;
-        float s = (float)p/q;           //can change
+    float s = (float)p/q;            //can change
         System.out.println(s);
     }
 }
