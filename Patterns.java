fb762428189d279c981a5f3e1624d6d8edf5f202
IamPawan777 <iampawanbisht7@gmail.com>
2023-02-20 03:11:17 +0530

    20-02 || 3:10

--- Patterns.java
+++ Patterns.java
@@ -1,3 +1,5 @@
+import javax.xml.transform.Source;
+
 // public class Patterns {
 //     public static void main(String[] args) {
 //         int n = 5;
@@ -9,14 +11,201 @@
 
 
 
+// public class Patterns {
+//     public static void main(String[] args) {
+//         int n = 5;
+//         for (int i=0; i<n; i++) {
+//             for(int j=0; j<n; j++) {
+//                 System.out.print("* ");
+//             }
+//             System.out.println();       //new line
+//         }
+//     }    
+// }
+
+
+// public class Patterns {
+//     public static void main(String[] args) {
+        
+//         int n = 5;
+//         for(int i=0; i<n;  i++) {           //0 to 4 (0 1 2 3 4)
+//             for(int j=0; j<n; j++) {
+//                 if(i==0 || i==n-1 || j==0 || j==n-1)
+//                     System.out.print("*");          //don't keep space
+//                 else
+//                     System.out.print(" ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+
+// public class Patterns {
+//     public static void main(String[] args) {
+//        int n=5;
+//        for(int i=0; i<n; i++) {
+//             for(int j=0; j<n; j++) {
+//                 if(i==0 || i==n-1 || j==0 || j==n-1 || i==(n-1)/2 || j==(n-1)/2)
+//                     System.out.print("*");
+//                 else 
+//                     System.out.print(" ");
+//             }
+//             System.out.println();
+//        }
+//     }
+// }
+
+
+
+
+
+// public class Patterns {
+//     public static void main(String[] args) {
+//        int n=5;
+//        for(int i=0; i<n; i++) {
+//         for(int j=0; j<n; j++) {
+//             if(i==0 || i==(n-1)/2 || j==0 || j==n-1 ) 
+//                 System.out.print("*");
+//             else
+//                 System.out.print(" ");
+//         }
+//         System.out.println();
+//        }
+//     }
+// }
+
+
+
+
+
+// public class Patterns {
+//     public static void main(String[] args) {
+//        int n=5;
+//        for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 if(i==(n-1)/2 || j==0 || j==n-1)
+//                     System.out.print("*");
+//                 else    
+//                     System.out.print(" ");
+//             }
+//             System.out.println();
+//        }
+//     }
+// }
+
+
+
+
+
+// public class Patterns {
+//     public static void main(String[] args) {
+//        int n=7;
+//        for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 if(i==0 || i==n-1 || j==(n-1)/2)
+//                     System.out.print("*");
+//                 else
+//                     System.out.print(" ");
+//             }  
+//             System.out.println();      
+//        }
+//       }
+// }
+
+
+
+
+
+// public class Patterns {
+//     public static void main(String[] args) {
+//        int n=5;
+//        for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 if(i==0 || i==(n-1)/2 || i==n-1 || j==0)
+//                     System.out.print("*");
+//                 else
+//                     System.out.print(" ");
+//             }
+//             System.out.println();
+//        }
+//     }
+// }
+
+
+
+
+
+
+// public class Patterns {
+//     public static void main(String[] args) {
+//        int n=5;
+//        for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 if(i==0 || i==(n-1)/2 || j==0)
+//                     System.out.print("*");
+//                 else
+//                     System.out.print(" ");
+//             }
+//             System.out.println();
+//        }
+//     }
+// }
+
+
+
+
+// public class Patterns {
+//     public static void main(String[] args) {
+//     int n=5;
+//     for (int i = 0; i < n; i++) {
+//         for (int j = 0; j < n; j++) {
+//             if(i==0 && j>0 && j<n-1  ||  j==0 && i>0 && i<n-1  ||  i==n-1 && j>0 && j<n-1  ||  j==n-1 && i>0 && i<n-1)
+//                 System.out.print("*");
+//             else
+//                 System.out.print(" ");
+//         }  
+//         System.out.println();
+//     }
+//     }
+// }
+
+
+
+
+// //A
+// public class Patterns {
+//         public static void main(String[] args) {
+//             int n=7;
+//             for (int i = 0; i < n; i++) {
+//                 for (int j = 0; j < n; j++) {
+//                     if(i==0 && j>0 && j<(n-1)/2 || j==0 && i>0 || j==(n-1)/2 && i>0 || i==(n-1)/2 && j<(n-1)/2)
+//                         System.out.print("*");
+//                     else
+//                         System.out.print(" ");
+//                 }
+//                 System.out.println();
+//             }
+//         }
+//     }
+
+
+
+
+//D
 public class Patterns {
     public static void main(String[] args) {
-        int n = 5;
-        for (int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                System.out.print("* ");
+        int n=7;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(j==0 || i==0 && j<n-1  || i==n-1 && j<n-1 || j==n-1 && i>0 && i<n-1 )
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
             }
-            System.out.println();       //new line
+            System.out.println();
         }
-    }    
+    }
 }
