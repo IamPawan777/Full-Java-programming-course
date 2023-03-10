297b059cf03385d55f9ca50e0565bbd4bd2a74b3
IamPawan777 <iampawanbisht7@gmail.com>
2023-03-11 03:21:26 +0530

    11-3-2023 || 3:21

---
+++ DecimalBinaryConv.java
@@ -0,0 +1,42 @@
+// //.....Binary to decimal conversion....
+
+// import java.util.Scanner;
+// public class DecimalBinaryConv {
+//     public static void main(String[] args) {
+//         Scanner scan = new Scanner(System.in);
+//         int binaryNum = scan.nextInt();
+//         int ans = 0;
+//         int power = 1;              //2^0 = 1
+//         while(binaryNum > 0) {
+//             int digit = binaryNum%10;
+//             ans += (digit * power);
+//             binaryNum/=10;
+//             power *= 2;
+//         }
+//         System.out.println(ans);
+//     }
+// }
+
+
+
+
+
+// //.....Decimal to Binary conversion....
+
+// import java.util.Scanner;
+// public class DecimalBinaryConv {
+//     public static void main(String[] args) {
+//         Scanner scan = new Scanner(System.in);
+//         int binaryNum = scan.nextInt();
+//         int ans = 0;
+//         int power = 1;
+//         while(binaryNum > 0) {
+//             int unit = binaryNum%2;
+//             ans += unit * power;
+//             binaryNum /= 2;
+//             power*=10;
+
+//         }
+//         System.out.println(ans);
+//     }
+// }
--- Patterns.java
+++ Patterns.java
@@ -276,21 +276,21 @@
 
 
 
-// .........16........
-class Patterns {
-    public static void main(String[] args) {
-        int n=7;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if(i==0 || j==0 || i+j<=n-1)
-                    System.out.print("*");
-                else 
-                    System.out.print(" ");
-            }
-            System.out.println();
-        }
-    }
-}
+// // .........16........
+// class Patterns {
+//     public static void main(String[] args) {
+//         int n=7;
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 if(i==0 || j==0 || i+j<=n-1)
+//                     System.out.print("*");
+//                 else 
+//                     System.out.print(" ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
 
 
 
@@ -483,3 +483,20 @@ class Patterns {
 
 
 
+
+
+// .........16........
+class Patterns {
+    public static void main(String[] args) {
+        int n=5;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i==j || i+j == n-1)
+                    System.out.print("*");
+                else    
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
