2d97df0379cc9f088331d8a906a0c3f252c2eb1d
IamPawan777 <iampawanbisht7@gmail.com>
2023-03-11 12:01:03 +0530

    11-3-2023 || 12:00

---
+++ ObjectClass.java
@@ -0,0 +1,22 @@
+// public class ObjectClass {
+//     String name = "john";
+//     int roll_no = 2;
+//     public static void main(String[] args) {
+//         ObjectClass obj = new ObjectClass();
+//         System.out.println("Name of student '"+obj.name+"' and roll no. is '"+obj.roll_no+"'.");
+//     }
+// }
+
+
+
+
+
+public class ObjectClass {
+    public static void main(String[] args) {
+        ObjectClass obj = new ObjectClass();
+        int a = 16;   
+        double store = Math.pow(a,.5);
+        double store1 = Math.sqrt(a);
+        System.out.println(store+"  "+store1);
+    }
+}
