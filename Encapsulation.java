e1da8fe937a58afb01ebe1777e144a0457fb00fa
IamPawan777 <iampawanbisht7@gmail.com>
2023-09-29 12:39:07 +0530

    updated

--- Encapsulation.java
+++ Encapsulation.java
@@ -1,28 +1,63 @@
-class Encapsulation {
-    private int age;
-    private String name;
+// class Encapsulation {
+//     private int age;
+//     private String name;
 
-    public int getAge() {
+//     public int getAge() {
+//         return age;
+//     }
+//     public void setAge(int age) {
+//             this.age = age;
+//     }
+
+
+//     public String getName() {
+//         return name;
+//     }
+//     public void setName(String name) {
+//         this.name = name;
+//     }
+
+//     public static void main(String[] args) {
+//         Encapsulation ob = new Encapsulation();
+//         ob.setAge(12);
+//         System.out.println("age: "+ob.getAge());
+//         ob.setName("Hariom");
+//         System.out.println("Name: "+ob.getName());
+
+//     }
+// }
+
+
+
+
+class Encapsulation {
+     double age  = 9;
+     String name;
+
+     int getAge(int age) {
         return age;
     }
-    public void setAge(int age) {
+     void getAge(int age) {
             this.age = age;
     }
 
 
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
+    //  String getName() {
+    //     return name;
+    // }
+    //  void setName(String name) {
+    //     this.name = name;
+    // }
 
     public static void main(String[] args) {
         Encapsulation ob = new Encapsulation();
-        ob.setAge(12);
-        System.out.println("age: "+ob.getAge());
-        ob.setName("Hariom");
-        System.out.println("Name: "+ob.getName());
+        // ob.setAge(12);
+        // System.out.println("age: "+ob.getAge());
+        // ob.setName("Hariom");
+        // System.out.println("Name: "+ob.getName());
+        ob.getAge(89);       
+        ob.getAge(22);
+
 
     }
 }
